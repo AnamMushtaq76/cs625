5d76e537c4fa28cee0aa2e1bd637c49014eb4804
AnamMushtaq76 <47882963+AnamMushtaq76@users.noreply.github.com>
2019-03-13 12:11:02 -0700

    git commit

---
+++ SCope.java
@@ -0,0 +1,27 @@
+public class SCope {
+
+   
+    public static void main(String[] args) {
+     int n,d,q;
+     n=10;
+     d=2;
+     if(d!=0 && (n%d)==0)
+         
+     
+         System.out.println(d+ "is factor of " +n);
+         d=0;
+       
+     if(d!=0 && (n%d)==0)
+         
+     
+         System.out.println(d+ "is factor of " +n);
+         d=0;
+         
+         if(d!=0 & (n%d)==0)
+         
+     
+         System.out.println(d+ "is factor of " +n);
+         d=0;
+    }
+    
+}
