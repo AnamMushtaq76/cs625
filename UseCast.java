ad7016255eacff3fbc0ae470ef823fb019db0011
AnamMushtaq76 <47882963+AnamMushtaq76@users.noreply.github.com>
2019-03-13 14:04:56 -0700

    git commit

---
+++ UseCast.java
@@ -0,0 +1,19 @@
+public class UseCast
+ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i;
+        for(i=0;i<5;i++)
+        {
+            System.out.println(i+"/3:"+i/3);
+            System.out.println(i+"/3 with fraction :" +(double) i/3);
+            System.out.println();
+            
+        }
+    }
+    
+}
