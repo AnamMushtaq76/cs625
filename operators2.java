7229c38e35e8b8044c041ff01214a7d0bc12ecc1
AnamMushtaq76 <47882963+AnamMushtaq76@users.noreply.github.com>
2019-03-14 10:02:55 -0700

    git commit

---
+++ operators2.java
@@ -0,0 +1,25 @@
+// Java program to illustrate
+//relational operators
+public class operators2
+{
+public static void main(String[] args)
+{
+int a=20,b=10;
+String x ="Thank", y ="Thank";
+int ar[]={1,2,3};
+int br[]={1,2,3};
+boolean condition = true;
+// various conditional operators
+System.out.println("a== b:"+(a==b));
+System.out.println("a< b:"+(a<b));
+System.out.println("a<= b:"+(a<=b));
+System.out.println("a> b:"+(a>b));
+System.out.println("a>= b:"+(a>=b));
+System.out.println("a!= b:"+(a!= b));
+// Arrays cannot be compared with
+// relational operators because objects
+// store references not the value
+System.out.println("x==y:" +(ar==br));
+System.out.println("condition==true :" + (condition == true));
+}
+}
