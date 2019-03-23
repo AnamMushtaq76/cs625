2094e07261c2227c3be7b7a6c98a8c5fd3ecbef2
AnamMushtaq76 <47882963+AnamMushtaq76@users.noreply.github.com>
2019-03-23 11:12:31 -0700

    git commit

---
+++ Guess2.java
@@ -0,0 +1,15 @@
+class Guess2
+{
+	public static void main(String args[])
+	throws java.io.IOException
+	    {
+	     char ch, answer  = 'K';
+	     System.out.println("I'm thinking of a letter between A and Z.");
+	     System.out.println("Can you guess it:");
+	     ch =(char) System.in.read(); 
+	     if (ch == answer)
+	     System.out.println("** Right **");
+	     else
+	     System.out.println("...Sorry, you're wrong.");
+	    }
+}
