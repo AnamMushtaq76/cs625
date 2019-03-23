c5c722d22cca4afcde59964373a2ecdfd873ec0c
AnamMushtaq76 <47882963+AnamMushtaq76@users.noreply.github.com>
2019-03-23 11:11:23 -0700

    git commit

---
+++ ForTest.java
@@ -0,0 +1,9 @@
+class ForTest {
+	public static void main(String args[]) 
+	throws java.io.IOException {
+	int i;
+	System.out.println("Press S to stop.");
+	for(i = 0; (char) System.in.read() != 'S'; i ++)
+	System.out.println("Pass #" + i);
+	}
+}
