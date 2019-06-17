6ef57a2e1d3395f52bfe0f26587c10be8e7fed14
AnamMushtaq76 <47882963+AnamMushtaq76@users.noreply.github.com>
2019-06-16 20:08:00 -0700

    git commit

---
+++ Block.java
@@ -0,0 +1,38 @@
+class Block {  
+  int a, b, c;  
+  int volume; 
+  
+  Block(int i, int j, int k) {  
+    a = i;  
+    b = j;  
+    c = k; 
+    volume = a * b * c; 
+  }  
+  
+  // Return true if ob defines same block. 
+  boolean sameBlock(Block ob) {  
+    if((ob.a == a) & (ob.b == b) & (ob.c == c)) return true;  
+    else return false;  
+  }  
+ 
+  // Return true if ob has same volume. 
+  boolean sameVolume(Block ob) { 
+    if(ob.volume == volume) return true; 
+    else return false; 
+  } 
+}  
+  
+class PassOb {  
+  public static void main(String args[]) { 
+    Block ob1 = new Block(10, 2, 5);  
+    Block ob2 = new Block(10, 2, 5);  
+    Block ob3 = new Block(4, 5, 5);  
+  
+    System.out.println("ob1 same dimensions as ob2: " + 
+                       ob1.sameBlock(ob2));  
+    System.out.println("ob1 same dimensions as ob3: " + 
+                        ob1.sameBlock(ob3));  
+    System.out.println("ob1 same volume as ob3: " + 
+                       ob1.sameVolume(ob3));  
+  }  
+}
